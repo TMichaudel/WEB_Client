e9b14dbc4927bdef36c5ccec4e8f331d3e56d689
TMichaudel <thibaud.michaudel@gmail.com>
2015-06-15 10:27:50 +0200

    Commit

---
+++ src/web_client/WEB_Client.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package web_client;
+
+/**
+ *
+ * @author Thibaud
+ */
+public class WEB_Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
